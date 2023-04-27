708169eb67097208e470ddc199b85c882c72fe9b
Pedro Sperandio <126828311+pedroluc4as@users.noreply.github.com>
2023-04-27 15:23:51 -0300

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, duplicate_ignore
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,6 +11,7 @@ class MeuApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // ignore: prefer_const_constructors
     return MaterialApp(
       home: PaginaInicial(),
     );
@@ -21,21 +24,25 @@ class PaginaInicial extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("  i     ❤️     JAY   "),
-        backgroundColor: Color.fromARGB(255, 0, 0, 0),
-
-      ),
-      body: Container(
-        alignment: Alignment.center,
-        color: Colors.purple[800],
-        child:ElevatedButton(
-          onPressed: () {},
-          child: Text("->❌<-",
-          style: TextStyle(fontSize: 35),
-          ),
+        appBar: AppBar(
+          title: Text("WhatsApp"),
+          backgroundColor: Color.fromARGB(255, 1, 219, 48),
         ),
-      ),
-    );
+        body: ListView(
+          children: [
+            ListTile(
+              title: Text("Meu Bem 💫 💜"),
+              subtitle: Row(children: [
+                Icon(Icons.image_rounded, size: 20),
+                Text("Photo")
+              ]),
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    "https://pps.whatsapp.net/v/t61.24694-24/328783685_1352476615596241_7740447452440029101_n.jpg?ccb=11-4&oh=01_AdQy77GmGfPy6HQp5A2CPEdf1CBkmHZvk-NeaAnQbzSAoA&oe=6457D140"),
+              ),
+              trailing: Text("1:52 pm"),
+            ),
+          ],
+        ));
   }
 }
